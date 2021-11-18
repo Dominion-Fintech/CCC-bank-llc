f7618327463a78e101ceed421a65a782ed44fc72
Bru ndifon <bruline1996@gmail.com>
2021-11-18 11:39:55 +0000

    this is my first commit

---
+++ id.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this script is to be used to sign up new users onto the server
+
+
+>>
+ comm
+ you have to make sure all due processes are followed before using this script
+
+All new users details mmust be stored in the storage provided for backup reasons
+
+Any other details should be cmmented here
+
+comm
+
